34e6fe2ff59f0cc61102cd39e9237baed53f0651
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-10-21 15:20:01 +1100

    asafad
    dsadasd

---
+++ Assets/Tests/SimpleTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SimpleTests
+{
+    [Test]
+    public void SimpleUnityTesy()
+    {
+        Assert.IsTrue(false, "FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE ");
+    }
+
+    [UnityTest]
+    public IEnumerator UnityTest()
+    {
+        yield return new WaitForFixedUpdate();
+        Assert.IsTrue(false, "FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE FAILURE ");
+    }
+}
